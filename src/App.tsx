b30f38404324b92640ef6e7fd2a830d87d0edac5
Daniel_Ouver <DANIELOUVERNEY67@GMAIL.COM>
2026-07-28 21:55:01 -0300

    style: criação inicial do layout da pokedex com tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import PokedexCardListWithData from "./components/PokedexCard";
 
 function App() {
-  return (
-    <>
-      <PokedexCardListWithData/>
-    </>
-  );
+  return <PokedexCardListWithData />;
 }
 
 export default App;
--- src/components/PokedexCard.tsx
+++ src/components/PokedexCard.tsx
@@ -1,3 +1,5 @@
+import "tailwindcss";
+
 import { useState } from "react";
 
 import withDataFetch from "./DataFetch";
@@ -32,37 +34,69 @@ const PokedexCard = ({ title, pokemon, fetchPokemon }: any) => {
   };
 
   return (
-    <div>
-      <h1>{title}</h1>
+    <body className="min-h-screen bg-blue-500 flex items-center justify-center text-center">
+      <div className="w-150 h-150 bg-red-500/90 rounded-2xl shadow-2xl p-2">
+        <h1 className="text-4xl p-6 text-white [text-shadow:1px_4px_0_black]">
+          <b>{title.toUpperCase()}</b>
+        </h1>
 
-      <input
-        type="text"
-        placeholder="ex: Número ID / Nome"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        //Função para fazer o Enter funcionar quando estamos pesquisando o pokemon
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            handleSearch();
-          }
-        }}
-      />
-      <button onClick={handleSearch}>Buscar</button>
+        <input
+          type="text"
+          placeholder="ex: Número ID / Nome"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          //Função para fazer o Enter funcionar quando estamos pesquisando o pokemon
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSearch();
+            }
+          }}
 
-      <div>
-        <h2>
-          {pokemon.name} - <b>#{pokemon.id}</b>
-        </h2>
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          className="bg-gray-300 rounded-2xl p-2 w-100"
+        />
+        <button
+          onClick={handleSearch}
+          className="bg-gray-900 rounded-full text-white w-10 p-2 cursor-pointer"
+        >
+          🔎
+        </button>
 
-        {pokemon.types.map((tipo: any) => (
-          <h2 key={tipo.slot}>{tipo.type.name}</h2>
-        ))}
+        <div className="bg-white p-7 mx-auto m-7 w-100 rounded-bl-4xl border-3">
+          <h2 className="text-xl">
+            <b>#{pokemon.id}</b> - <span>{pokemon.name.toUpperCase()}</span>
+          </h2>
+          <img
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+            className="mx-auto size-50"
+          />
+
+          <div className="grid grid-cols-2 gap-2">
+            {pokemon.types.map((tipo: any) => (
+              <h2
+                key={tipo.slot}
+                className="bg-gray-400 p-1.5 rounded-full border-2 border-gray-700"
+              >
+                {tipo.type.name.toUpperCase()}
+              </h2>
+            ))}
+          </div>
+        </div>
+
+        <button
+          onClick={handlePrevious}
+          className="bg-gray-900 text-white rounded-xl p-2 w-30 mr-5 cursor-pointer"
+        >
+          Anterior
+        </button>
+        <button
+          onClick={handleNext}
+          className="bg-gray-900 text-white rounded-xl p-2 w-30 ml-5 cursor-pointer"
+        >
+          Próximo
+        </button>
       </div>
-
-      <button onClick={handlePrevious}>⬅Anterior</button>
-      <button onClick={handleNext}>Próximo➡</button>
-    </div>
+    </body>
   );
 };
 
